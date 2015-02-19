b2adf55b529447c10c6fa85c9282c9906d373fb0
nemanjan00 <nemanjan00@gmail.com>
2015-02-19 01:38:33 +0100

    Config

--- index.php
+++ index.php
@@ -4,9 +4,10 @@ include("./Tor.Class.php");
 $tor = new tor();
 
 $domain = "torlinkbgs6aabns.onion";
+$clone_domain = "ktpufypzla45vzfo.onion";
 
 if($_SERVER["REQUEST_METHOD"] == "GET"){
-	echo str_replace($domain, "ktpufypzla45vzfo.onion", $tor->get("http://$domain".$_SERVER["REQUEST_URI"]));
+	echo str_replace($domain, $clone_domain, $tor->get("http://$domain".$_SERVER["REQUEST_URI"]));
 }
 else
 {
